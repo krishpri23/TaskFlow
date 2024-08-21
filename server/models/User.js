87240f308c5cf612be760fc9bfd4b08982b90695
Krishnapriya <17042821+krishpri23@users.noreply.github.com>
2024-08-21 12:17:22 -0700

    added mongodb

---
+++ server/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  roles: [
+    {
+      type: String,
+      default: "Employee", // if frontend does not have a role, it will be employee by default
+    },
+  ],
+  active: {
+    type: Bolean,
+    default: true, // if frontend does not have active employee, it will be true by default
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
