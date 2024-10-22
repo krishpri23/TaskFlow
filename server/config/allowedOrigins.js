928f207ab921470f7ea86e57938339fbafa89d04
Krishnapriya <17042821+krishpri23@users.noreply.github.com>
2024-10-21 21:52:52 -0700

    added allowedOrigins

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://taskflow-n1sh.onrender.com",
+];
 
 module.exports = allowedOrigins;
