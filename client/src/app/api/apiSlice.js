1e4329be78b2a2b5ac280dea56ef633773848cc0
Krishnapriya <17042821+krishpri23@users.noreply.github.com>
2024-11-24 16:11:19 -0800

    changed base url

--- client/src/app/api/apiSlice.js
+++ client/src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://taskflow-backend-86ff.onrender.com",
+  baseUrl: "https://taskflow-api-ag4g.onrender.com",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
